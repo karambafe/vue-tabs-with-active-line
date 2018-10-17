ddfd01b1d169817f3ef1f65d5f7085d967d6737a
Vitaly Blokhin <karambafe@yahoo.com>
2018-10-17 18:37:54 +0300

    update rollup config for demo

--- build/demo.rollup.config.js
+++ build/demo.rollup.config.js
@@ -1,33 +1,10 @@
 import vue from 'rollup-plugin-vue';
-import hash from 'rollup-plugin-hash';
 import uglify from 'rollup-plugin-uglify-es';
-import copy from 'rollup-plugin-copy';
 import html from 'rollup-plugin-fill-html';
-
-// export default {
-//   input: 'demo-src/main.js',
-//   output: {
-//     file: 'demo/build.js',
-//     format: 'iife',
-//   },
-//   plugins: [
-//     vue({
-//       css: true,
-//       compileTemplate: true,
-//     }),
-//     uglify(),
-//     hash({
-//       dest: 'demo/main.[hash:8].js',
-//     }),
-//     copy({
-//       "demo-src/index.html": "demo/index.html",
-//     }),
-//   ],
-// };
 
 export default {
   input: 'demo-src/main.js',
-  dest: 'demo/bundle-[hash].js',
+  dest: 'docs/bundle-[hash].js',
   plugins: [
     vue({
       css: true,
