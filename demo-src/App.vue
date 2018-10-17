b7aadaac9eceafe9454730561d3df836b8813a8e
Vitaly Blokhin <karambafe@yahoo.com>
2018-10-17 17:19:04 +0300

    update styles for demo page

--- demo-src/App.vue
+++ demo-src/App.vue
@@ -1,16 +1,42 @@
 <template>
-  <div>
+  <div class="container">
     <h1>Vue tabs with active line</h1>
-    <example />
+    <simple />
   </div>
 </template>
 
 <script>
-import Example from '../example/Example.vue';
+import Simple from '../example/Simple.vue';
 
 export default {
   components: {
-    Example,
+    Simple,
   },
 };
 </script>
+
+<style>
+  body {
+    margin: 0;
+  }
+
+  h1 {
+    text-align: center;
+  }
+
+  .container {
+    font-family: "Avenir", Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    color: #2c3e50;
+
+    min-width: 320px;
+    max-width: 1000px;
+    margin: 0 auto;
+    padding: 20px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+</style>
+
